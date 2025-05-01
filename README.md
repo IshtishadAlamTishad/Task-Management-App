07f93b4aee2eefd8329ec9ae617265afe0c8b70f
IshtishadAlamTishad <tishadalam86@gmail.com>
2025-05-02 01:18:26 +0600

    added icon,updated validation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Task-Management-App
 Our web technology course's project 
+
+in this project, we are using 
+
+HTML for web UI structure
+CSS for styling the web pages
+Javascript for validation
+PHP for validation
--- login.js
+++ login.js
@@ -1,11 +1,14 @@
-document.getElementById("togglePassword").addEventListener("click", function () {
+document.getElementById('loginTitleTxt').innerHTML = "Task Management";
+
+
+document.getElementById("togglePassword").addEventListener("click",function () {
     const password = document.getElementById("password");
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
-    this.textContent = type === "password" ? "👁️" : "🙈";
+    this.innerHTML = type === "password" ? "👁️" : "🙈";
   });
   
-  document.getElementById("loginForm").addEventListener("submit", function (e) {
+  document.getElementById("loginForm").addEventListener("submit",function (e) {
     e.preventDefault();
   
     const email = document.getElementById("email").value.trim();
@@ -22,6 +25,7 @@ document.getElementById("togglePassword").addEventListener("click", function () 
       message.style.color = "green";
       message.textContent = "Login successful!";
       alert("Login Successful!");
+      window.location.href = "profile.html";
     } else {
       message.style.color = "red";
       message.textContent = "Invalid email or password.";
