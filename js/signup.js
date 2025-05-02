d6e743869531cfacbcb81ea3c37421412e7875d8
IshtishadAlamTishad <tishadalam86@gmail.com>
2025-05-02 12:53:33 +0600

    updated Structure folder,added signup page

---
+++ js/signup.js
@@ -0,0 +1,19 @@
+const upload = document.getElementById('uploads');
+const img = document.getElementById('imgs');
+
+document.getElementById('upTxt').innerHTML = "SignUp";
+
+img.addEventListener('click',() => {
+    upload.click();
+});
+
+upload.addEventListener('change',function(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = function() {
+        img.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+});
